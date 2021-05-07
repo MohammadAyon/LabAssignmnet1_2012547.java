bc4233506c40871e84d8ae922a84fe9418cd27d9
mohammad-Ayon <mohammadayon6969@gmail.com>
2021-05-07 22:00:38 +0600

    just doing shit that I can complete

---
+++ src/Codeforce1A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Codeforce1A {
+  public static void main(String[] args) {
+    int[] arr = new int[1000000];
+    Scanner in = new Scanner(System.in);
+    int t=in.nextInt();
+    int flag=1;
+    while(t>0){
+      for(int i=0;i<arr.length;i++){
+      
+      }
+      t--;
+    }
+  
+  }
+}
